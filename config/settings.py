e642f9ab8b468d0d07104ec0512b57d61337b08e
Muhammad Hasnain <hasnainalamglt143@gmail.com>
2025-07-21 08:01:08 -0700

    added  allowed host for vercel

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bsm7jt6-m8s%o820nv91unu!4e92#p!uokj@4uylhldsv-%cqd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["http://127.0.0.1:8000","https://movie-recommendation-2ylbpuc3m.vercel.app"]
+ALLOWED_HOSTS = ["127.0.0.1:8000","movie-recommendation-2ylbpuc3m.vercel.app"]
 
 
 # Application definition
